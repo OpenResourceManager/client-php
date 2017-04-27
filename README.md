44364ac1edeeb23bca3a78f339a91304ae47f62d
Alex Markessinis <markea@sage.edu>
2017-04-27 10:32:08 -0400

    initalized

---
+++ README.md
@@ -0,0 +1,2 @@
+# ORM Client for PHP
+
---
+++ src/ORM.php
@@ -0,0 +1,22 @@
+<?php namespace OpenResourceManager\Client;
+
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 4/27/17
+ * Time: 10:29 AM
+ */
+
+/**
+ *  ORM Client
+ *
+ * Communicates with an ORM API.
+ *
+ * @author Alex Markessinis
+ */
+class ORM
+{
+
+
+
+}
