63ad729978147acc291c2d462413d1748a93ba3f
Alex Markessinis <markea@sage.edu>
2017-06-15 20:05:21 -0400

    added store and delete methods to AddressClient

--- src/Client/Addresses.php
+++ src/Client/Addresses.php
@@ -46,12 +46,69 @@ class Addresses extends Client
      *
      * Gets an address by it's ID.
      *
-     * @param int $id
+     * @param $id
      * @return \Unirest\Response
      */
-    public function get($id = 0)
+    public function get($id)
     {
         return parent::get($id);
     }
 
+    /**
+     * Store Address
+     *
+     * Store an address.
+     *
+     * @param $account_id
+     * @param $identifier
+     * @param $username
+     * @param $addressee
+     * @param $organization
+     * @param $line_1
+     * @param $line_2
+     * @param $city
+     * @param $state_id
+     * @param $state_code
+     * @param $zip
+     * @param $country_id
+     * @param $country_code
+     * @param $latitude
+     * @param $longitude
+     * @return \Unirest\Response
+     */
+    public function store($account_id, $identifier, $username, $addressee, $organization, $line_1, $line_2, $city, $state_id, $state_code, $zip, $country_id, $country_code, $latitude, $longitude)
+    {
+        $fields = [];
+
+        if (!is_null($account_id)) $fields['account_id'] = $account_id;
+        if (!is_null($identifier)) $fields['identifier'] = $identifier;
+        if (!is_null($username)) $fields['username'] = $username;
+        if (!is_null($addressee)) $fields['addressee'] = $addressee;
+        if (!is_null($organization)) $fields['$organization'] = $organization;
+        if (!is_null($line_1)) $fields['line_1'] = $line_1;
+        if (!is_null($line_2)) $fields['line_2'] = $line_2;
+        if (!is_null($city)) $fields['city'] = $city;
+        if (!is_null($state_id)) $fields['state_id'] = $state_id;
+        if (!is_null($state_code)) $fields['state_code'] = $state_code;
+        if (!is_null($zip)) $fields['zip'] = $zip;
+        if (!is_null($country_id)) $fields['country_id'] = $country_id;
+        if (!is_null($country_code)) $fields['country_code'] = $country_code;
+        if (!is_null($latitude)) $fields['latitude'] = $latitude;
+        if (!is_null($longitude)) $fields['longitude'] = $longitude;
+
+        return $this->_post($fields);
+    }
+
+    /**
+     * Delete Address
+     *
+     * Delete an address by it's ID.
+     *
+     * @param $id
+     * @return \Unirest\Response
+     */
+    public function delete($id)
+    {
+        return $this->_del(['id' => $id]);
+    }
 }
